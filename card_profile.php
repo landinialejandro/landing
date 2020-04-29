f71b6090084900e241c3e85c662a72f44face6ec
Alejandro Landini - nbkani <landinialejandro@gmail.com>
2020-04-29 15:21:27 -0300

    add card profile

---
+++ card_profile.php
@@ -0,0 +1,44 @@
+ <!-- profile -->
+ <div class="col-lg-6">
+        <!-- Widget: user widget style 1 -->
+        <div class="card card-widget widget-user">
+          <!-- Add the bg color to the header using any of the bg-* classes -->
+          <div class="widget-user-header bg-info">
+            <h3 class="widget-user-username">Alejandro Landini</h3>
+            <h5 class="widget-user-desc">Founder &amp; CEO</h5>
+          </div>
+          <div class="widget-user-image">
+            <img class="img-circle elevation-2" src="dist/dist_L/logo/Face.png" alt="User Avatar">
+          </div>
+          <div class="card-footer">
+            <div class="row">
+              <div class="col-sm-4 border-right">
+                <div class="description-block">
+                  <h5 class="description-header"><span class="counter" id="counter-sales" data-elapsetime="8" data-startcountvalue="199" data-endcountvalue="122">0</span></h5>
+                  <span class="description-text">VENTAS</span>
+                </div>
+                <!-- /.description-block -->
+              </div>
+              <!-- /.col -->
+              <div class="col-sm-4 border-right">
+                <div class="description-block">
+                  <h5 class="description-header"><span class="counter" id="counter-followers" data-elapsetime="8" data-startcountvalue="88" data-endcountvalue="96">0</span></h5>
+                  <span class="description-text">SEGUIDORES</span>
+                </div>
+                <!-- /.description-block -->
+              </div>
+              <!-- /.col -->
+              <div class="col-sm-4">
+                <div class="description-block">
+                  <h5 class="description-header"><span class="counter" id="counter-productos" data-elapsetime="8" data-startcountvalue="4" data-endcountvalue="7">0</span></h5>
+                  <span class="description-text">PRODUCTOS</span>
+                </div>
+                <!-- /.description-block -->
+              </div>
+              <!-- /.col -->
+            </div>
+            <!-- /.row -->
+          </div>
+        </div>
+        <!-- /.widget-user -->
+      </div>
--- index.php
+++ index.php
@@ -18,66 +18,20 @@ include("header.php");
   </div><!-- /.container-fluid -->
 </div>
 <!-- /.content-header -->
-
 <!-- Main content -->
 <div class="content">
   <div class="container">
     <div class="row">
-      <!-- profile -->
-      <div class="col-lg-6">
-        <!-- Widget: user widget style 1 -->
-        <div class="card card-widget widget-user">
-          <!-- Add the bg color to the header using any of the bg-* classes -->
-          <div class="widget-user-header bg-info">
-            <h3 class="widget-user-username">Alejandro Landini</h3>
-            <h5 class="widget-user-desc">Founder &amp; CEO</h5>
-          </div>
-          <div class="widget-user-image">
-            <img class="img-circle elevation-2" src="dist/dist_L/logo/Face.png" alt="User Avatar">
-          </div>
-          <div class="card-footer">
-            <div class="row">
-              <div class="col-sm-4 border-right">
-                <div class="description-block">
-                  <h5 class="description-header"><span class="counter" id="counter-sales" data-elapsetime="8" data-startcountvalue="199" data-endcountvalue="122">0</span></h5>
-                  <span class="description-text">VENTAS</span>
-                </div>
-                <!-- /.description-block -->
-              </div>
-              <!-- /.col -->
-              <div class="col-sm-4 border-right">
-                <div class="description-block">
-                  <h5 class="description-header"><span class="counter" id="counter-followers" data-elapsetime="8" data-startcountvalue="88" data-endcountvalue="96">0</span></h5>
-                  <span class="description-text">SEGUIDORES</span>
-                </div>
-                <!-- /.description-block -->
-              </div>
-              <!-- /.col -->
-              <div class="col-sm-4">
-                <div class="description-block">
-                  <h5 class="description-header"><span class="counter" id="counter-productos" data-elapsetime="8" data-startcountvalue="4" data-endcountvalue="7">0</span></h5>
-                  <span class="description-text">PRODUCTOS</span>
-                </div>
-                <!-- /.description-block -->
-              </div>
-              <!-- /.col -->
-            </div>
-            <!-- /.row -->
-          </div>
-        </div>
-        <!-- /.widget-user -->
-      </div>
-      <?php 
+      <?php
+      include "card_profile.php";
       include "products/snippetsGen/card.php";
       include "products/lat/card.php";
       include "products/erp_sp/card.php";
       include "products/tnz/card.php";
       include "products/mpi/card.php";
       ?>
-      <!-- /.row -->
-    </div><!-- /.container -->
+    </div>
   </div>
-  <!-- /.content -->
   <?php
   include("footer.php");
   ?>
