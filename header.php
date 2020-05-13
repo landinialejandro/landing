b417c584ec4ac9be58990142d2a939ac763de29d
landinialejandro <landinialejandro@gmail.com>
2020-05-13 16:55:53 -0300

    add google tag

--- header.php
+++ header.php
@@ -12,7 +12,13 @@
   <meta property="og:url" content="http://www.landini.com.ar/">
   <meta property="og:site_name" content="Landini software develope">
   <meta property="og:description" content="Desarrollo de aplicaciones web optimizadas para dispositivos móviles, más rápido y mucho menos costoso.">
-
+  <!-- Google Tag Manager -->
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-5JHG5VM');</script>
+  <!-- End Google Tag Manager -->
   <title>Landini</title>
   <link id="browser_favicon" rel="shortcut icon" href="logo.png">
   <link rel="stylesheet" href="dist/dist/css/adminlte.min.css">
@@ -46,6 +52,10 @@
 </head>
 
 <body class="hold-transition layout-top-nav">
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5JHG5VM"
+  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
   <div class="wrapper">
 
     <!-- Navbar -->
