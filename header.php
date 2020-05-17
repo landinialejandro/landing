e3bdf14f6c4742aaf882708afdb4f1daf958c0bf
Alejandro Landini - nbkani <landinialejandro@gmail.com>
2020-05-17 10:59:05 -0300

    add google tag

--- header.php
+++ header.php
@@ -12,13 +12,15 @@
   <meta property="og:url" content="http://www.landini.com.ar/">
   <meta property="og:site_name" content="Landini software develope">
   <meta property="og:description" content="Desarrollo de aplicaciones web optimizadas para dispositivos móviles, más rápido y mucho menos costoso.">
-  <!-- Google Tag Manager -->
-  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-5JHG5VM');</script>
-  <!-- End Google Tag Manager -->
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-85010303-2"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-85010303-2');
+  </script>
+
   <title>Landini</title>
   <link id="browser_favicon" rel="shortcut icon" href="logo.png">
   <link rel="stylesheet" href="dist/dist/css/adminlte.min.css">
@@ -53,8 +55,7 @@
 
 <body class="hold-transition layout-top-nav">
   <!-- Google Tag Manager (noscript) -->
-  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5JHG5VM"
-  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5JHG5VM" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
   <!-- End Google Tag Manager (noscript) -->
   <div class="wrapper">
 
